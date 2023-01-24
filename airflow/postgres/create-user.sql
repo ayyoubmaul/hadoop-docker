2600b58db5c8182465a828475c74bef0bcde8858
ayyoubmaul <underestimate70@gmail.com>
2023-01-25 01:49:01 +0700

    airflow: remove airflow

--- airflow/postgres/create-user.sql
+++
@@ -1,6 +0,0 @@
-USE postgres
-
-CREATE USER airflow;
-ALTER USER airflow WITH PASSWORD 'airflow';
-CREATE DATABASE airflow;
-GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
--- airflow/run_airflow.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# build images that listed in docker-compose-build.yml
-docker-compose -f docker-compose-build.yml build
