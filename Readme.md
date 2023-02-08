976f2dfad7ba9e5c93d4c1bbdf8a6ecfbc84b82b
ayyoubmaul <underestimate70@gmail.com>
2023-02-08 09:50:43 +0700

    hadoop: Add mapred script

--- Readme.md
+++ Readme.md
@@ -34,7 +34,7 @@ hadoop jar /opt/hadoop-3.3.1/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar -
 2. hdfs-site.xml default and description [here](https://hadoop.apache.org/docs/r3.3.1/hadoop-project-dist/hadoop-hdfs/hdfs-default.xml)
 3. mapred-site.xml default and description [here](https://hadoop.apache.org/docs/r3.3.1/hadoop-mapreduce-client/hadoop-mapreduce-client-core/mapred-default.xml)
 4. yarn-site.xml default and description [here](https://hadoop.apache.org/docs/r3.3.1/hadoop-yarn/hadoop-yarn-common/yarn-default.xml)
-5. To calculate YARN and MapReduce meory configuration [here](https://docs.cloudera.com/HDPDocuments/HDP2/HDP-2.0.9.0/bk_installing_manually_book/content/rpm-chap1-11.html)
+5. To calculate YARN and MapReduce memory configuration [here](https://docs.cloudera.com/HDPDocuments/HDP2/HDP-2.0.9.0/bk_installing_manually_book/content/rpm-chap1-11.html)
 
 # Note
 Please start everything from `run_cluster.sh` because the base image created from this step
---
+++ map_reduce/analyze_map_reduce.py
@@ -0,0 +1,44 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+
+class SalesAverage(MRJob):
+
+    def steps(self):
+        return [
+            MRStep(
+                mapper=self.mapper_by_product,
+                reducer=self.reducer_average_price
+            ),
+            MRStep(
+                reducer=self.reducer_sort_average
+            )
+        ]
+
+    def mapper_by_product(self, _, line):
+        data = line.split(',')
+
+        if data[3] != 'Price Each' and data[3] != '':
+            product = data[1]
+            price = float(data[3])
+            yield product, price
+
+    def reducer_average_price(self, key, value):
+        total_data = 0
+        total_price = 0
+
+        for price in value:
+            total_data += 1
+            total_price += price
+
+        yield None, ('{:.2f}'.format(float(total_price/total_data)), key)
+
+    def reducer_sort_average(self, _, values):
+        for average, key in sorted(values, key=lambda x: (float(x[0]), x[1])):
+            with open('/Users/ayyoub-maulana/gitRepo/hadoop-in-docker/data/output_sales_mapred.csv', 'a') as f:
+                f.write(f'{key}, {average}\n')
+
+            yield key, average
+
+if __name__ == '__main__':
+    SalesAverage.run()
